dfca84eada9b08ed0ef9bfd9174e74133c4cb092
jahadtariq <muhammadjahadtariq@gmail.com>
2023-01-12 08:55:07 +0500

    Update site name

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/home/jahad/Documents/Projects/UI-UX/metaverse/app/page":["/home/jahad/Documents/Projects/UI-UX/metaverse/styles/globals.css"]},"/home/jahad/Documents/Projects/UI-UX/metaverse/app/layout.js":["/home/jahad/Documents/Projects/UI-UX/metaverse/styles/globals.css"]}
